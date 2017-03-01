04b35b2bbef9bc467d04cdf1a2e2fe9fb012e2d3
Brandon <brandonkr3w@gmail.com>
2017-03-01 00:43:23 -0700

    Final Coding

--- MillerChapter4Assignment/MillerChapter4Assignment.cs
+++ MillerChapter4Assignment/MillerChapter4Assignment.cs
@@ -11,18 +11,13 @@ namespace MillerChapter4Assignment
         static void Main(string[] args)
         {
 
-
             Console.WriteLine("Enter employee name: ");
             string employeeName = Console.ReadLine();
             Console.WriteLine("Total Sales: ");
             decimal totalSales = Convert.ToDecimal(Console.ReadLine());
             Employee employeeOne = new Employee();
             employeeOne.TotalSales = totalSales;
-            Console.WriteLine("\nAmount on Commission: {0}", employeeOne.CommissionTotal);
-            Console.WriteLine("\nFederal Tax: {0}", employeeOne.FederalTotal);
-            Console.WriteLine("\nRetirement: {0}", employeeOne.RetireTotal);
-            Console.WriteLine("\nSocial Security: {0}", employeeOne.SocialSecurityTotal); 
-            Console.WriteLine("\nAmount Taken Out: {0}", employeeOne.MoneyOut);
+            Console.WriteLine(employeeOne.ToString());
             Console.ReadKey();
         }
     }
