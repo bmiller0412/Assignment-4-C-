4c98f9beff7a9a5d4d7af39978e75b1ccb2eb4b0
Brandon <brandonkr3w@gmail.com>
2017-02-28 23:17:09 -0700

    Add project files.

---
+++ MillerChapter4Assignment/Employee.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MillerChapter4Assignment
+{
+    //Class Created
+    public class Employee
+    {
+        //Variables Declared
+        public decimal Commission = 0.07M;
+        public decimal FederalTax = 0.18M;
+        public decimal Retirement = 0.10M;
+        public decimal SocialSecurity = 0.06M;
+        public decimal totalSales;
+        public decimal commissionTotal;
+        public decimal federalTotal;
+        public decimal retireTotal;
+        public decimal socialSecurityTotal;
+        public decimal moneyOut;
+        public decimal takenPay;
+        public string employeeName;
+
+        //Default Constructor
+        public Employee()
+        {
+        }
+
+        //Employee
+        public Employee(string Name)
+        {
+            employeeName = Name;
+        }
+
+        public Employee(string employeeNameIn, decimal employeeSalesIn, decimal employeeCommissionIn,
+            decimal employeeFederalIn, decimal employeeReitreIn, decimal employeeSocialIn, decimal employeeMoneyIn,
+            decimal employeePayIn)
+        {
+            employeeName = employeeNameIn;
+            totalSales = employeeSalesIn;
+            commissionTotal = employeeCommissionIn;
+            federalTotal = employeeFederalIn;
+            socialSecurityTotal = employeeSocialIn;
+            moneyOut = employeeMoneyIn;
+            takenPay = employeePayIn;
+        }
+
+        //Get and Set
+        public string EmployeeName
+        {
+            get
+            {
+                return employeeName;
+            }
+            set
+            {
+                employeeName = value;
+            }
+        }
+
+        public decimal TotalSales
+        {
+            get
+            {
+                return totalSales;
+            }
+            set
+            {
+                totalSales = value;
+            }
+        }
+
+        public decimal CommissionTotal
+        {
+            get
+            {
+                return commissionTotal;
+            }
+            set
+            {
+                commissionTotal = value;
+            }
+        }
+
+        public decimal FederalTotal
+        {
+            get
+            {
+                return federalTotal;
+            }
+            set
+            {
+                federalTotal = value;
+            }
+        }
+
+        public decimal RetireTotal
+        {
+            get
+            {
+                return retireTotal;
+            }
+            set
+            {
+                retireTotal = value;
+            }
+        }
+
+        public decimal SocialSecurityTotal
+        {
+            get
+            {
+                return socialSecurityTotal;
+            }
+            set
+            {
+                socialSecurityTotal = value;
+            }
+        }
+
+        public decimal MoneyOut
+        {
+            get
+            {
+                return moneyOut;
+            }
+            set
+            {
+                moneyOut = value;
+            }
+        }
+
+        public decimal TakenPay
+        {
+            get
+            {
+                return takenPay;
+            }
+            set
+            {
+                takenPay = value;
+            }
+        }
+        //Get Total Pay
+        public decimal GetCommissionTotal()
+        {
+            return totalSales * Commission;
+        }
+
+        //Federal Taxes
+        public decimal GetFederalTaxes()
+        {
+            return totalSales * FederalTax;
+        }
+
+        //Retirement Investments
+        public decimal GetRetirement()
+        {
+            return totalSales * Retirement;
+        }
+
+        //Social Security
+        public decimal GetSocialSecurity()
+        {
+            return totalSales * SocialSecurity;
+        }
+
+        //Money Taken Out
+        public decimal GetMoneyOut()
+        {
+            return retireTotal + federalTotal + socialSecurityTotal;
+        }
+
+        //Take Home Pay
+        public decimal GetTakenPay()
+        {
+            return TotalSales = moneyOut;
+        }
+
+        //Override string
+        public override string ToString()
+        {
+            return
+                string.Format(
+                    "Employee: {0} \nTotal Sales: {1} \nCommission: {2} \nFederal Tax: {3} \nRetirement: {4} \nSocial Security: {5} \nTake Home: {6}",
+                    employeeName, totalSales, commissionTotal, federalTotal, retireTotal, socialSecurityTotal);
+        }
+    }
+}
---
+++ MillerChapter4Assignment/MillerChapter4Assignment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MillerChapter4Assignment
+{
+    class MillerChapter4Assignment
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter employee name: ");
+            string employeeName = Console.ReadLine();
+            Console.WriteLine("Total Sales: ");
+            decimal totalSales = Convert.ToDecimal(Console.ReadLine());
+            Employee employeeOne = new Employee();
+            Console.WriteLine("\nAmount on Commission: {0}", employeeOne.CommissionTotal);
+            Console.WriteLine("\nFederal Tax: {0}", employeeOne.FederalTotal);
+            Console.WriteLine("\nRetirement: {0}", employeeOne.RetireTotal);
+            Console.WriteLine("\nSocial Security: {0}", employeeOne.SocialSecurityTotal); 
+            Console.WriteLine("\nAmount Taken Out: {0}", employeeOne.MoneyOut);
+            Console.WriteLine("\nTaken Pay: {0}", employeeOne.TakenPay);
+            Console.ReadKey();
+        }
+    }
+}
