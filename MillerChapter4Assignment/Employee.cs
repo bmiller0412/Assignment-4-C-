4903cee12e2fe1d264419591d85a4841ef150351
Brandon <brandonkr3w@gmail.com>
2017-02-28 23:53:47 -0700

    meh meh meh

--- MillerChapter4Assignment/Employee.cs
+++ MillerChapter4Assignment/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,80 +11,41 @@ namespace MillerChapter4Assignment
     public class Employee
     {
         //Variables Declared
-        public decimal Commission = 0.07M;
-        public decimal FederalTax = 0.18M;
-        public decimal Retirement = 0.10M;
-        public decimal SocialSecurity = 0.06M;
-        public decimal TotalSales;
-        public decimal CommissionTotal;
-        public decimal FederalTotal;
-        public decimal RetireTotal;
-        public decimal SocialSecurityTotal;
-        public decimal MoneyOut;
-        public decimal TakenPay;
-        public string EmployeeName;
+        public decimal Commission { get; set; } = 0.07M;
+        public decimal FederalTax { get; set; } = 0.18M;
+        public decimal Retirement { get; set; } = 0.10M;
+        public decimal SocialSecurity { get; set; } = 0.06M;
+        public decimal TotalSales { get; set; }
+        public decimal CommissionTotal { get; set; }
+        public decimal FederalTotal { get; set; }
+        public decimal RetireTotal { get; set; }
+        public decimal SocialSecurityTotal { get; set; }
+        public decimal MoneyOut { get; set; }
+        public decimal TakenPay { get; set; }
+        public string EmployeeName { get; set; }
 
         //Default Constructor
         public Employee()
         {
         }
 
-        //Employee
-        public Employee(string Name)
-        {
-            EmployeeName = Name;
-        }
-
-        public Employee(string employeeNameIn, decimal employeeSalesIn, decimal employeeCommissionIn,
-            decimal employeeFederalIn, decimal employeeReitreIn, decimal employeeSocialIn, decimal employeeMoneyIn,
-            decimal employeePayIn)
-        {
-            EmployeeName = employeeNameIn;
-            TotalSales = employeeSalesIn;
-            CommissionTotal = employeeCommissionIn;
-            FederalTotal = employeeFederalIn;
-            SocialSecurityTotal = employeeSocialIn;
-            MoneyOut = employeeMoneyIn;
-            TakenPay = employeePayIn;
-        }
-
-        
         //Get Total Pay
-        public decimal GetCommissionTotal()
-        {
-            return TotalSales * Commission;
-        }
-
+        public decimal GetCommissionTotal() => TotalSales * (decimal) Commission;
+       
         //Federal Taxes
-        public decimal GetFederalTaxes()
-        {
-            return TotalSales * FederalTax;
-        }
+        public decimal GetFederalTaxes() => TotalSales * (decimal)FederalTax;
 
         //Retirement Investments
-        public decimal GetRetirement()
-        {
-            return TotalSales * Retirement;
-        }
+        public decimal GetRetirement() => TotalSales * (decimal) Retirement;
 
         //Social Security
-        public decimal GetSocialSecurity()
-        {
-            return TotalSales * SocialSecurity;
-        }
+        public decimal GetSocialSecurity() => TotalSales * (decimal)SocialSecurity;
 
         //Money Taken Out
-        public decimal GetMoneyOut()
-        {
-            return RetireTotal + FederalTotal + SocialSecurityTotal;
-        }
+        public decimal GetMoneyOut() => RetireTotal + FederalTotal + SocialSecurityTotal;
 
         //Take Home Pay
-        public decimal GetTakenPay()
-        {
-            return TotalSales = MoneyOut;
-        }
-
+        public decimal GetTakenPay() => TotalSales = MoneyOut;
         //Override string
         public override string ToString()
         {
